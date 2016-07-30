e5210f37a2f53e342e02390fde521c6ad5f5277f
Stewart Matheson <stew@rtmatheson.com>
2016-07-30 14:56:50 +1000

    Pulling map data down

--- app/scripts/scene/map-scene.js
+++ app/scripts/scene/map-scene.js
@@ -1,13 +1,31 @@
+const parsePolyString = polyString => {
+  console.log(polyString);
+  return polyString;
+}
+
+
 PixiGame.MapScene = function() {
     PIXI.Graphics.call(this);
 
     this.setup();
 };
 
+
 PixiGame.MapScene.constructor = PixiGame.GameScene;
 PixiGame.MapScene.prototype = Object.create(PIXI.Graphics.prototype);
 
 PixiGame.MapScene.prototype.setup = function() {
+  var url = "https://data.melbourne.vic.gov.au/api/views/4zfn-ezbh/rows.json?accessType=DOWNLOAD";
+  var loader = PIXI.loader;
+  loader.add('roadData', url);
+  loader.once('complete', function (event, resource) {
+    var roadShapes = resource.roadData.data.data.map(road => {
+      return parsePolyString(road[8])
+    });
+    
+  });
+
+  loader.load();
 }
 
 PixiGame.MapScene.prototype.update = function() {
